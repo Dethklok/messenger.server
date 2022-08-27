f1ce70a5bb0858be2381e5e05a4329eca6fb10b9
ispaschansky <ispaschanskyi@infotech-cloudsolutions.com>
2022-08-27 10:24:29 +0400

    Added DefaultMessageProjection

--- src/main/kotlin/org/pegasus/messenger/server/repository/MessageRepository.kt
+++ src/main/kotlin/org/pegasus/messenger/server/repository/MessageRepository.kt
@@ -1,8 +1,12 @@
 package org.pegasus.messenger.server.repository
 
 import org.pegasus.messenger.server.model.Message
+import org.pegasus.messenger.server.model.projection.DefaultMessageProjection
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
 
-@RepositoryRestResource
+@RepositoryRestResource(
+    path = "messages",
+    collectionResourceRel = "messages",
+    excerptProjection = DefaultMessageProjection::class)
 interface MessageRepository : JpaRepository<Message, Long>
