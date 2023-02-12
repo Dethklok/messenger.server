59eabdf6b61d4cd1e00c5ffc123632134e5eee98
ispaschansky <ispaschanskyi@infotech-cloudsolutions.com>
2023-02-12 16:14:56 +0400

    Refactor: clean

--- src/main/kotlin/org/pegasus/messenger/server/adapter/data/user/ShortUserProjection.kt
+++
@@ -1,7 +0,0 @@
-package org.pegasus.messenger.server.adapter.data.user
-
-import org.pegasus.messenger.server.application.port.ShortUserPort
-import org.springframework.data.rest.core.config.Projection
-
-@Projection(name = "ShortUser", types = [JpaUser::class])
-interface ShortUserProjection : ShortUserPort
