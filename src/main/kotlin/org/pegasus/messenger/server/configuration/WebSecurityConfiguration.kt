292e5e57d60197a7c7ba639269b46a0685659cb7
Dethklok <x.firedragon@gmail.com>
2023-09-30 20:20:41 +0400

    Update deprecated methods for spring security

--- src/main/kotlin/org/pegasus/messenger/server/configuration/WebSecurityConfiguration.kt
+++ src/main/kotlin/org/pegasus/messenger/server/configuration/WebSecurityConfiguration.kt
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.invoke
 import org.springframework.security.oauth2.server.resource.authentication.OpaqueTokenAuthenticationProvider
 import org.springframework.security.oauth2.server.resource.introspection.OpaqueTokenIntrospector
 import org.springframework.security.web.SecurityFilterChain
@@ -15,47 +16,48 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 @Configuration
 @EnableWebSecurity
 class WebSecurityConfiguration(
-  private val opaqueTokenIntrospector: OpaqueTokenIntrospector,
-  private val authenticationConverter: AuthenticationConverter
+    private val opaqueTokenIntrospector: OpaqueTokenIntrospector,
+    private val customAuthenticationConverter: AuthenticationConverter
 ) {
 
-  @Bean
-  fun filterChain(
-    http: HttpSecurity,
-  ): SecurityFilterChain {
-    http
-      .authorizeHttpRequests {
-        it
-          .requestMatchers("/wss-main").permitAll()
-          .anyRequest().authenticated()
-      }
-      .csrf().disable()
-      .cors()
-      .and()
-      .oauth2ResourceServer()
-      .opaqueToken()
-      .authenticationConverter(authenticationConverter)
+    @Bean
+    fun filterChain(
+        http: HttpSecurity,
+    ): SecurityFilterChain {
+        http {
+            authorizeHttpRequests {
+                authorize("/wss-main", permitAll)
+                authorize(anyRequest, authenticated)
+            }
+            csrf { disable() }
+            cors { }
+            oauth2ResourceServer {
+                opaqueToken {
+                    authenticationConverter = customAuthenticationConverter
+                }
+            }
+        }
 
-    return http.build()
-  }
+        return http.build()
+    }
 
-  @Bean
-  fun corsConfigurationSource(): CorsConfigurationSource {
-    val configuration = CorsConfiguration()
-    configuration.allowedOrigins = listOf("http://localhost:4200")
-    configuration.allowedMethods = listOf("OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
-    configuration.allowedHeaders = listOf("Content-Type", "Authorization")
-    configuration.allowCredentials = true
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = listOf("http://localhost:4200")
+        configuration.allowedMethods = listOf("OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
+        configuration.allowedHeaders = listOf("Content-Type", "Authorization")
+        configuration.allowCredentials = true
 
-    val source = UrlBasedCorsConfigurationSource()
-    source.registerCorsConfiguration("/**", configuration)
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
 
-    return source
-  }
+        return source
+    }
 
-  @Bean
-  fun opaqueTokenAuthenticationProvider() =
-    OpaqueTokenAuthenticationProvider(
-      opaqueTokenIntrospector
-    )
+    @Bean
+    fun opaqueTokenAuthenticationProvider() =
+        OpaqueTokenAuthenticationProvider(
+            opaqueTokenIntrospector
+        )
 }
